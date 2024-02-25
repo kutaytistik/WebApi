cd2be31e6bf1ea8eea12db1dd9ca43fa117174a4
Kutay Tıstık <kutaytistik99@gmail.com>
2024-02-26 00:16:48 +0300

    Add project files.

---
+++ Business/Abstracts/IBrandService.cs
@@ -0,0 +1,19 @@
+﻿using Entities.Concretes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Business.Dtos.Responses;
+using Business.Dtos.Requests;
+
+namespace Business.Abstracts
+{
+    public interface IBrandService
+    {
+        CreatedBrandResponse Add(CreateBrandRequest createBrandRequest);
+        List<GetAllBrandResponse> GetAll();
+
+    }
+    //responses and request
+}
